b35e52a4a91af8bb71ac92c8f61656a6d11ae798
Noah-Mack-01 <noahnewmanmack@gmail.com>
2025-03-27 10:18:03 -0400

    Add memoization

--- src/main/kotlin/com/noahnewmanmack/derivatives_model/service/BinomialModel.kt
+++ src/main/kotlin/com/noahnewmanmack/derivatives_model/service/BinomialModel.kt
@@ -8,37 +8,60 @@ import kotlin.math.exp
 
 @Service
 class BinomialModel: PricingModel<BinomialContext> {
+
+    var map :MutableMap<Double,Double> = mutableMapOf()
+    var decayFactor: Double = 0.0
     override fun calculate(context: BinomialContext): Double {
-        if (context.periodsLeft == 0) {
+        map.clear()
+        decayFactor = exp(-context.riskFree * context.annualizedPeriod)
+        return calculateHelper(context)
+    }
+
+    fun calculateHelper(context: BinomialContext): Double {
+        val periodsLeft = context.periodsLeft
+        if (periodsLeft == 0) {
             return when (context.optionType) {
                 OptionType.PUT -> maxOf(0.0, context.strikePrice - context.stockPrice)
-                OptionType.CALL -> return maxOf(0.0, context.stockPrice - context.strikePrice)
+                OptionType.CALL -> maxOf(0.0, context.stockPrice - context.strikePrice)
             }
         }
         else {
-            val upSwing = calculate(BinomialContext(
-                context.stockPrice * context.upSwingModifier,
-                context.strikePrice,
-                context.riskFree,
-                context.upSwingProbability,
-                context.upSwingModifier,
-                context.downSwingModifier,
-                context.periodsLeft - 1,
-                context.annualizedPeriod,
-                context.optionType
-            ))
-            val downSwing = calculate(BinomialContext(
-                context.stockPrice * context.downSwingModifier,
-                context.strikePrice,
-                context.riskFree,
-                context.upSwingProbability,
-                context.upSwingModifier,
-                context.downSwingModifier,
-                context.periodsLeft - 1,
-                context.annualizedPeriod,
-                context.optionType
-            ))
-            return (upSwing * context.upSwingProbability + downSwing * (1 - context.upSwingProbability)) * exp(-context.riskFree * context.annualizedPeriod)
+
+            if (map.containsKey(context.strikePrice)) {
+                println("Cache hit, ${map[context.strikePrice]}")
+                return map[context.strikePrice]!!
+            };
+
+            val upSwing = calculateHelper(
+                BinomialContext(
+                    context.stockPrice * context.upSwingModifier,
+                    context.strikePrice,
+                    context.riskFree,
+                    context.upSwingProbability,
+                    context.upSwingModifier,
+                    context.downSwingModifier,
+                    context.periodsLeft - 1,
+                    context.annualizedPeriod,
+                    context.optionType
+                )
+            )
+            val downSwing = calculateHelper(
+                BinomialContext(
+                    context.stockPrice * context.downSwingModifier,
+                    context.strikePrice,
+                    context.riskFree,
+                    context.upSwingProbability,
+                    context.upSwingModifier,
+                    context.downSwingModifier,
+                    context.periodsLeft - 1,
+                    context.annualizedPeriod,
+                    context.optionType
+                )
+            )
+
+            val payoutValue = ((upSwing * context.upSwingProbability) + downSwing * (1 - context.upSwingProbability)) * decayFactor
+            map[context.strikePrice] = payoutValue
+            return payoutValue
         }
     }
 }
